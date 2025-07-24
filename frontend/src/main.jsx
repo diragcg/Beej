14c892bd141781fa2a5a07f946b746a86f08ea05
diragcgit <viksitkrishisankalpabhiyan@gmail.com>
2025-07-24 21:41:34 +0530

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+// This is a placeholder for the main entry point
+// It will be implemented during actual development
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
